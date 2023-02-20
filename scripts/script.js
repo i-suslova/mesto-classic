a2f1b0ef0ad162c34852e8483432cf475df43f24
Irina <i.sus2011@yandex.ru>
2023-02-20 19:50:07 +0300

    git commit -m "creat seaction"

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,51 @@
 const buttonEdit = document.querySelector(".profile__button-edit");
-const popup = document.querySelector(".popup");
-const buttonClose = document.querySelector(".popup__button-close");
-const formElement = document.querySelector(".popup__form");
+const popupProfile = document.querySelector(".popup_profile");
+const buttonProfileClose = document.querySelector(".popup__button-close_profile");
+const formProfile = document.querySelector(".popup__form-profile");
 const nameInput = document.querySelector("#input-name");
 const jobInput = document.querySelector("#input-job");
 const profileTitle = document.querySelector(".profile__title");
 const profileSubtitle = document.querySelector(".profile__subtitle");
 
-//замена значения в форме из значений в профиле
-//открытие попапа, добавляя модификатор
-buttonEdit.addEventListener("click", function () {
-  nameInput.value = profileTitle.textContent;
-  jobInput.value = profileSubtitle.textContent;
-  popup.classList.add("popup_opened");
-});
+const buttonProfileAdd = document.querySelector(".profile__button-add");
+const popupPicture = document.querySelector(".popup_picture");
+const buttonPictureClose = document.querySelector(".popup__button-close_picture");
+const pictureInput = document.querySelector("#input-picture");
+const linkInput = document.querySelector("#input-link");
+
+
+//создаем отдельную именованную функцию открытия попапа
+function openFormPopup(element) {
+  element.classList.add("popup_opened");
+}
 
 //создаем отдельную именованную функцию закрытия попапа
 function closeFormPopup(element) {
   element.classList.remove("popup_opened");
 }
 
-//закрытие попапа, используя именованную функцию
-buttonClose.addEventListener("click", function () {
-  closeFormPopup(popup);
+//замена значения в форме из значений в профиле
+//открытие попапа-profile, используя именованную функцию
+buttonEdit.addEventListener("click", function () {
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
+  openFormPopup(popupProfile);
 });
+
+//закрытие попапа-profile, используя именованную функцию
+buttonProfileClose.addEventListener("click", function () {
+  closeFormPopup(popupProfile);
+});
+
+//открытие попапа-template, используя именованную функцию
+buttonProfileAdd.addEventListener("click", function() {
+  openFormPopup(popupPicture);
+})
+
+//закрытие попапа-template, используя именованную функцию
+buttonPictureClose.addEventListener("click", function () {
+  closeFormPopup(popupPicture);
+})
 
 // Обработчик «отправки» формы
 function handleFormSubmit(evt) {
@@ -31,13 +53,18 @@ function handleFormSubmit(evt) {
 
   profileTitle.textContent = nameInput.value;
   profileSubtitle.textContent = jobInput.value;
-  //закрытие попапа, используя именованную функцию
-  closeFormPopup(popup);
+
+  //закрытие попапа-profile, используя именованную функцию
+  closeFormPopup(popupProfile);
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener("submit", handleFormSubmit);
+formProfile.addEventListener("submit", handleFormSubmit);
+
+
+function
+
 
 // ссылки на картинки
 const initialCards = [
