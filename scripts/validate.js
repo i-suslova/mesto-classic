165d2e7df5be3c8f3d335ba4b991c32662aa9532
Irina <i.sus2011@yandex.ru>
2023-03-20 14:48:25 +0300

    refactor:  кнопка отправки валидной формы

--- scripts/validate.js
+++ scripts/validate.js
@@ -3,30 +3,39 @@ const config = {
   inputSelector: ".popup__input",
   submitButtonSelector: ".popup__button",
   inactiveButtonClass: "popup__button_disabled",
-  inputErrorClass: "popup__input_type_error", //red
-  errorClass: "popup__error_visible", //
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible",
 };
 
-//onst maxformElement = document.querySelector(".popup__form"); //form
-//const formInput = formElement.querySelector(".popup__input"); //input
-//const formMaxError = formElement.querySelector(".popup__error"); //span blue
-// const formError = formElement.querySelector(`.${formInput.id}-error`); //span blue
-//console.log(maxformElement);
-
+//добавляем спан с ошибкой и подчеркиваем красной чертой
 const showInputError = (formElement, inputElement, errorMessage) => {
-  const formError = formElement.querySelector(`.${inputElement.id}-error`); //span blue
+  const formError = formElement.querySelector(`.${inputElement.id}-error`);
 
-  inputElement.classList.add("popup__input_type_error");
+  inputElement.classList.add(config.inputErrorClass);
   formError.textContent = errorMessage;
-  formError.classList.add("popup__error_visible");
+  formError.classList.add(config.errorClass);
 };
 
+//удаляем спан с ошибкой и убираем красную черту
 const hideInputError = (formElement, inputElement) => {
-  const formError = formElement.querySelector(`.${inputElement.id}-error`); //span blue
+  const formError = formElement.querySelector(`.${inputElement.id}-error`);
 
-  inputElement.classList.remove("popup__input_type_error");
-  formError.classList.remove("popup__error_visible");
+  inputElement.classList.remove(config.inputErrorClass);
+  formError.classList.remove(config.errorClass);
   formError.textContent = "";
+};
+
+//показываем неработающую кнопку отправки формы
+const disableButton = (buttonElement) => {
+  buttonElement.classList.add(config.inactiveButtonClass);
+  buttonElement.classList.remove(config.submitButtonSelector);
+  buttonElement.disabled = true;
+};
+//показываем работающую кнопку отправки формы
+const enableButton = (buttonElement) => {
+  buttonElement.classList.add(config.submitButtonSelector);
+  buttonElement.classList.remove(config.inactiveButtonClass);
+  buttonElement.disabled = false;
 };
 
 // Функция, которая проверяет валидность поля
@@ -40,22 +49,33 @@ const checkInputValidity = (formElement, inputElement) => {
   }
 };
 
-//функция, проверяющая корректность содержимого на валидность
-//const hasInvalidInput = (inputList) => {
-//  return inputList.some((inputElement) => !inputElement.validity.valid);
-//};
-
+//функция,котрая принимает массив полей
 const hasInvalidInput = (inputList) => {
-  return inputList.some((inputElement) => {
-    return !inputElement.validity.valid;
-  });
+  //методом some находим невалидный input
+  return inputList.some((input) => !input.validity.valid);
+};
+
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    disableButton(buttonElement);
+  } else {
+    enableButton(buttonElement);
+  }
 };
 
 const setEventListeners = (formElement) => {
   const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
+  const buttonElement = formElement.querySelector(config.submitButtonSelector);
+
+  // вызов функции toggleButtonState до ввода данных в поля,
+  // для недоступного состояния кнопки при загрузке страницы
+  toggleButtonState(inputList, buttonElement);
+
   inputList.forEach((inputElement) => {
     inputElement.addEventListener("input", () => {
       checkInputValidity(formElement, inputElement);
+      // вызов функции toggleButtonState и передадим ей массив полей и кнопку
+      toggleButtonState(inputList, buttonElement);
     });
   });
 };
@@ -63,15 +83,8 @@ const setEventListeners = (formElement) => {
 const enableValidation = (config) => {
   const formList = Array.from(document.querySelectorAll(config.formSelector));
   formList.forEach((formElement) => {
-    setEventListeners(formElement);
+    setEventListeners(formElement, config);
   });
 };
 
 enableValidation(config);
-
-console.log("123");
-//console.log(!formInput.validity.valid);
-// console.log(formList);
-console.log(enableValidation);
-//сonsole.log(inputList);
-//сonsole.log(formList);
